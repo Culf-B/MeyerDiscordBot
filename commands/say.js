0f54bf5bee2e43bf8f8b86fa373bb210feda1e33
Culf-B <ulfbirk05@gmail.com>
2022-04-25 17:36:56 +0200

    Bugfix and comments

--- commands/say.js
+++ commands/say.js
@@ -8,27 +8,43 @@ module.exports = {
 		.addNumberOption(option =>
 		option.setName("what")
 			.setDescription("What have you got? (as a number)")
-			.setRequired(true)),
+			.setRequired(false)),
 	async execute(interaction, manager) {
 		if (interaction.channel.name === "meyer"){
 			if (manager.isUserAwaited(interaction.user.id)){
 
 				let game = manager.games[manager.awaitingUsers[interaction.user.id]]
 				if (game.awaitedAction == "say"){
-					let roll = interaction.options.getNumber("what").toString()
-					let rollValue = [parseFloat(roll[0]),parseFloat(roll[1])].sort().reverse()
-					let rollText = `${rollValue[0]}${rollValue[1]}`
-					console.log(rollText, rollValue)
-					let value = numbers[rollValue[0]][rollValue[1]].toString()
-					console.log(value)
-					if (names[value]){
-						rollText += ` (${names[value]})`
+					
+					if (interaction.options.getNumber("what")){
+					
+						// Get what the user says they rolled
+						let roll = interaction.options.getNumber("what").toString()
+						let rollList = [parseFloat(roll[0]),parseFloat(roll[1])].sort().reverse()
+						let rollText = `${rollList[0]}${rollList[1]}`
+						let rollValue = numbers[rollList[0]][rollList[1]]
+						
+						// Check if the value rolled has a name in the "numbers.json" file
+						if (names[rollValue.toString()]){
+							rollText += ` (${names[rollValue.toString()]})`
+						}
+
+						let nextPlayer = game.say(rollValue)
+						// Give the turn to the next player
+						delete(manager.awaitingUsers[interaction.user.id])
+						manager.awaitingUsers[nextPlayer.id] = game.owner.id
+						// Response
+						await interaction.reply(`${interaction.user.tag} says they got ${rollText}.\n${nextPlayer.tag} do you believe it?`)
+
+					} else {
+						// If they said nothing, it means "That or thereover"
+						let nextPlayer = game.thereover()
+						// Give the turn to the next player
+						delete(manager.awaitingUsers[interaction.user.id])
+						manager.awaitingUsers[nextPlayer.id] = game.owner.id
+						// Response
+						await interaction.reply(`${interaction.user.tag} said "That or thereover". ${nextPlayer.tag} do you think it is over?`)
 					}
-					let nextPlayer = game.say(rollValue)
-					delete(manager.awaitingUsers[interaction.user.id])
-					console.log(nextPlayer)
-					manager.awaitingUsers[nextPlayer.id] = game.owner.id
-					await interaction.reply(`${interaction.user.tag} says they got ${rollText}.\n${nextPlayer.tag} do you believe it?`)
 				}
 			}
 			else {
